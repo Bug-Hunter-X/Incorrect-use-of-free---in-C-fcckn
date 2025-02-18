6c379e9e2ad9a7d8f1ce1a35a4c8dbddadd2da4a
BugHunter-X <askzenai@gmail.com>
2025-02-18 18:24:05 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,1 @@
+int main() {    int x = 10;    int *ptr = &x;    *ptr = 20;    int y = *ptr;    printf("%d", y); // Prints 20    // free(ptr); // Removed: No longer attempting to free a stack-allocated variable. }
